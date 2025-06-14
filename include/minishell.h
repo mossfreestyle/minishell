2f908937949717f1409f80ecee6bb45729dac131
mossfreestyle <mosswassims3@gmail.com>
2025-06-14 16:56:56 +0200

    diff struct

--- include/minishell.h
+++ include/minishell.h
@@ -6,7 +6,7 @@
 /*   By: rwassim <rwassim@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/02 09:57:51 by rwassim           #+#    #+#             */
-/*   Updated: 2025/06/14 14:55:38 by rwassim          ###   ########.fr       */
+/*   Updated: 2025/06/14 16:51:11 by rwassim          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,8 @@
 # include <stdlib.h>
 # include <string.h>
 # include <unistd.h>
+
+# define MAX_PIPES 1024
 
 typedef enum e_token_type
 {
@@ -53,6 +55,27 @@ typedef struct s_command
 	int					heredoc_fd;
 	struct s_command	*next;
 }						t_command;
+
+typedef struct s_env
+{
+	char				*name;
+	char				*value;
+	struct s_env		*next;
+}						t_env;
+
+typedef struct s_pipe
+{
+	int					n_pipes;
+	int					pipefd[MAX_PIPES][2];
+}						t_pipe;
+
+typedef struct s_shell
+{
+	int					exit_status;
+	char				*pwd;
+	t_env				*env_vars;
+	t_pipe				pipeline;
+}						t_shell;
 
 t_token					*new_token(t_token_type type, char *value);
 t_token					*meta_token(t_token *tokens, char *line, int *i);
